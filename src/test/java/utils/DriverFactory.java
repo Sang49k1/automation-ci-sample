f8c6abb5d53c8492970d24e7faa614cf9355b710
SangNT5 <sangnt5@vng.com.vn>
2026-04-17 13:16:38 +0700

    fix driver factory syntax

--- src/test/java/utils/DriverFactory.java
+++ src/test/java/utils/DriverFactory.java
@@ -1,22 +1,28 @@
+package utils;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
+
 import java.net.URL;
 
-public static WebDriver getDriver() {
-    try {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless=new");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("window-size=1920,1080");
+public class DriverFactory {
 
-        return new RemoteWebDriver(
-                new URL("http://host.docker.internal:4444/wd/hub"),
-                options
-        );
+    public static WebDriver getDriver() {
+        try {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("window-size=1920,1080");
 
-    } catch (Exception e) {
-        throw new RuntimeException(e);
+            return new RemoteWebDriver(
+                    new URL("http://host.docker.internal:4444/wd/hub"),
+                    options
+            );
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
