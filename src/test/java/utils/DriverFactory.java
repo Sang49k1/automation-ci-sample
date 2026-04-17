c559af00afd7ab780083da5d9d515d6f884f63e0
SangNT5 <sangnt5@vng.com.vn>
2026-04-17 13:09:03 +0700

    update driverFactory

--- src/test/java/utils/DriverFactory.java
+++ src/test/java/utils/DriverFactory.java
@@ -1,21 +1,22 @@
-package utils;
-
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.net.URL;
 
-public class DriverFactory {
-
-    public static WebDriver getDriver() {
-        WebDriverManager.chromedriver().setup();
-
+public static WebDriver getDriver() {
+    try {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("window-size=1920,1080");
 
-        return new ChromeDriver(options);
+        return new RemoteWebDriver(
+                new URL("http://host.docker.internal:4444/wd/hub"),
+                options
+        );
+
+    } catch (Exception e) {
+        throw new RuntimeException(e);
     }
 }
